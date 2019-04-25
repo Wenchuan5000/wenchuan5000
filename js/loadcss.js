5a987ee9dee52130e0f88ec8fd0b6f60a923f240
Wenchuan <zhao_wenchuan@icloud.com>
2019-04-25 08:30:56 +0800

    update

---
+++ js/loadcss.js
@@ -0,0 +1,17 @@
+document.write('\
+\
+<link rel="stylesheet" href="./css/master.css">\
+<link rel="stylesheet" href="./css/root.css">\
+<link rel="stylesheet" href="./css/code.css">\
+\
+<script type="text/x-mathjax-config">\
+  MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});\
+</script>\
+<script type="text/javascript"
+  src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">\
+</script>\
+\
+<meta charset="utf-8">\
+<meta name="viewport" content="width=device-width, initial-scale=1.0">\
+\
+');
