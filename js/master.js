8f0dd1f940cb0140ef6ccc6bc4142bdbdace24bf
Wenchuan <zhao_wenchuan@icloud.com>
2019-05-22 10:23:01 +0800

    update

--- js/master.js
+++ js/master.js
@@ -1,15 +1,15 @@
 
 // begin hide navition
 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("nav").style.top = "0";
-  } else {
-    document.getElementById("nav").style.top = "-100px";
-  }
-  prevScrollpos = currentScrollPos;
-}
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+// var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("nav").style.top = "0";
+//   } else {
+//     document.getElementById("nav").style.top = "-100px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
 
 // end hide navition
