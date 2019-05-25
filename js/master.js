125177a9bce895199812abd06723796b3bb34838
Wenchuan <zhao_wenchuan@icloud.com>
2019-05-25 11:17:10 +0800

    solve the sidenav!

--- js/master.js
+++ js/master.js
@@ -15,13 +15,26 @@
 // end hide navition
 
 function closeNav() {
-  document.getElementById("sidenav2").style.left = "-300px";
+  document.getElementById("sidenav").style.left = "-300px";
   document.getElementById("btn-openNav").style.opacity = "1";
   document.getElementById("btn-closeNav").style.display = "none";
 }
 
 function openNav() {
-  document.getElementById("sidenav2").style.left = "0";
+  document.getElementById("sidenav").style.left = "0";
   document.getElementById("btn-openNav").style.opacity = "0.9";
   document.getElementById("btn-closeNav").style.display = "block";
 }
+
+function forceOpenNav() {
+  document.getElementById("sidenav").style.left = "0";
+}
+
+
+window.addEventListener("resize", function() {
+    if (window.matchMedia("(max-width: 1400px)").matches) {
+        window.onresize = closeNav;
+    } else {
+        window.onresize = forceOpenNav;
+    }
+});
